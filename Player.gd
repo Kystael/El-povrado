a8bc5f4ea7cfc5fd46119a69f2738098f694b491
Amaury Fauchille <amaury.fauchille.dev@gmail.com>
2019-10-06 19:27:05 +0200

    Add sprite rotation to Player

--- Player.gd
+++ Player.gd
@@ -1,9 +1,5 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export var speed = 100
 
 # Called when the node enters the scene tree for the first time.
@@ -15,12 +11,16 @@ func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		$AnimatedSprite.flip_v = false
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		$AnimatedSprite.flip_v = true
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 
